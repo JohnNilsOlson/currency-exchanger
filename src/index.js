757febfee83e7ff8922eda882cfb4dcab60463c8
JohnNils Olson <johnnils@gmail.com>
2020-07-03 11:34:42 -0700

    add fetch api call

--- src/index.js
+++ src/index.js
@@ -4,6 +4,15 @@ import './styles.css';
 import $ from "jquery";
 
 import { Exchange } from './../src/exchange';
+// import { getRates } from './../src/exchange-service'; 
+
+function getElements(response) {
+  if (response) {
+    console.log(response);
+  } else {
+    $('#error').text('There was an error handling your request');
+  }
+}
 
 $(document).ready(function() {
 
@@ -19,5 +28,18 @@ $(document).ready(function() {
     exchange = new Exchange(from, to, value);
     console.log(exchange);
 
+    fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+      .then(function(response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json();
+      })
+      .catch(function(error) {
+        return error;
+      })
+      .then(function(jsonifiedResponse) {
+        getElements(jsonifiedResponse);
+      });
   });
 });
