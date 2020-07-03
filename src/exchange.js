b5c00fa0f87b5ec1a89d16e86398c8eeee690d16
JohnNils Olson <johnnils@gmail.com>
2020-07-03 14:27:13 -0700

    add display output to convert function

--- src/exchange.js
+++ src/exchange.js
@@ -15,7 +15,7 @@ export class Exchange {
   async convert() {
     let apiResponse = await getRates();
     if (!apiResponse) {
-      $('#error').text('There was an error handling your request');
+      $('#output').text('Sorry, there was an error handling your request.');
     } else {
       let rateArray = Object.entries(apiResponse.conversion_rates);
       rateArray.forEach((i) => {
@@ -23,6 +23,7 @@ export class Exchange {
           this.code = i[0];
           this.conversionRate = i[1];
           this.convertedValue = (this.value * this.conversionRate);
+          $('#output').text(this.value + ' ' + this.from + ' is equal to ' + this.convertedValue + ' ' + this.to);
         }
       });    
     }
