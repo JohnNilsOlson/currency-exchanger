cd7cefb0615b6290d6dd38e7f84269842814fb59
JohnNils Olson <johnnils@gmail.com>
2020-07-03 08:23:06 -0700

    add submit function for inputs, create exchange object

--- src/index.js
+++ src/index.js
@@ -2,3 +2,22 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
+
+import { Exchange } from './../src/exchange';
+
+$(document).ready(function() {
+
+  let exchange;
+
+  $('#exchange-amount').submit(function(event) {
+    event.preventDefault();
+
+    const from = $('#currency-type-from').val();
+    const to = $('#currency-type-to').val();
+    const value = $('#amount').val();
+
+    exchange = new Exchange(from, to, value);
+    console.log(exchange);
+
+  });
+});
