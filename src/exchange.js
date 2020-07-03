8c899a09062ca531f9af67fe04bc802b0d98fff2
JohnNils Olson <johnnils@gmail.com>
2020-07-03 12:36:41 -0700

    add exchange-service and separate business logic from UI logic

--- src/exchange.js
+++ src/exchange.js
@@ -1,7 +1,20 @@
+import $ from "jquery";
+
+import { getRates } from './../src/exchange-service';
+
 export class Exchange {
   constructor(from, to, value) {
     this.from = from;
     this.to = to;
     this.value = value;
+  }
+
+  async findRate() {
+    let apiResponse = await getRates();
+    if (!apiResponse) {
+      $('#error').text('There was an error handling your request');
+    } else {
+      console.log(apiResponse.conversion_rates);
+    }
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,16 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
 
-import { Exchange } from './../src/exchange';
-// import { getRates } from './../src/exchange-service'; 
-
-function getElements(response) {
-  if (response) {
-    console.log(response);
-  } else {
-    $('#error').text('There was an error handling your request');
-  }
-}
+import { Exchange } from './../src/exchange'; 
 
 $(document).ready(function() {
 
@@ -28,18 +19,6 @@ $(document).ready(function() {
     exchange = new Exchange(from, to, value);
     console.log(exchange);
 
-    fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
-      .then(function(response) {
-        if (!response.ok) {
-          throw Error(response.statusText);
-        }
-        return response.json();
-      })
-      .catch(function(error) {
-        return error;
-      })
-      .then(function(jsonifiedResponse) {
-        getElements(jsonifiedResponse);
-      });
+    exchange.findRate();
   });
 });
